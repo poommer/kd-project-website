c56f375336920e396117f8563ecbc5875c591b3d
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2024-05-02 18:23:11 +0700

    updateRefaech

--- script.js
+++ script.js
@@ -147,6 +147,9 @@ function getLastData() {
             newLatest = latest.timestamp
             newLatestD = latest.DvID
             $('#update-gif').show();
+            $('#ChartL7d').remove()
+            $('#Chart7-dayNight').remove()
+            getChart_L7d(`https://script.google.com/macros/s/AKfycbwzyMQQGRdOvt52OLgqShInHrIwSyD6_yB2hGfEkqPfjAePEUiKEotUlhfCyCQgYPm4/exec?dateStart=${setDate(1).Start}&dateEnd=${setDate(1).End}`)
           }
 
           console.table(newLatest)
@@ -370,11 +373,10 @@ async function initMap() {
 $(document).ready(function () {
 
   
-
-
+  getChart_L7d(`https://script.google.com/macros/s/AKfycbwzyMQQGRdOvt52OLgqShInHrIwSyD6_yB2hGfEkqPfjAePEUiKEotUlhfCyCQgYPm4/exec?dateStart=${setDate(1).Start}&dateEnd=${setDate(1).End}`)
+  getLastData()
   setInterval(function () {
     $('#update-date').html('<span id="update-date" style="font-size:14px; color: #818181; margin-left:0.5rem;"><spanclass="spinner-grow" aria-hidden="true"></spanclass=>กำลังโหลดข้อมูล...</span>');
-    getChart_L7d(`https://script.google.com/macros/s/AKfycbwzyMQQGRdOvt52OLgqShInHrIwSyD6_yB2hGfEkqPfjAePEUiKEotUlhfCyCQgYPm4/exec?dateStart=${setDate(1).Start}&dateEnd=${setDate(1).End}`)
     getLastData()
   }, 8000);
 
